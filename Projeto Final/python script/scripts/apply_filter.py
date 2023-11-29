b1e8479834d3adc69f36bb4bcf3170366a027e60
Jonatan Hartmann <jonatanhartamann@gmail.com>
2023-11-29 12:19:22 -0300

    Corrigindo script aply_filter

--- Projeto Final/python script/scripts/apply_filter.py
+++ Projeto Final/python script/scripts/apply_filter.py
@@ -27,16 +27,26 @@ def filtro_pixel(matriz_file, kernel_file):
             deslocaLinha = -1
             deslocaColuna = -1
             while i < 9:
-                soma += kernel[i]*mem[(linha + deslocaLinha)*602 + (coluna + deslocaColuna)]
+                k = kernel[i]
+                m = mem[(linha + deslocaLinha)*602 + (coluna + deslocaColuna)]
+                res = k*m
+                input(f"{k}*{m} = {res}")
+                res = bin(res)
+                res = res[2: len(res)].zfill(8)
+                resb = res[0:len(res)-4].zfill(8)
+                resi = int(resb, 2)
+                # input(f"{soma} - {somab} - {somai}")
+                soma += resi
                 i += 1
                 deslocaColuna += 1
 
                 if deslocaColuna > 1:
                     deslocaColuna = -1
                     deslocaLinha += 1
-
+            # print(soma)
             soma = bin(soma)
-            soma = soma[2:len(soma)-5].zfill(8)
+            # input(f"{soma} - {soma[2:len(soma)].zfill(8)}")
+            soma = soma[2:len(soma)].zfill(8)
             mem_saida[(linha-1)*600 + (coluna-1)] = soma
 
             coluna += 1
